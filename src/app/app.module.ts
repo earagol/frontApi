d578b1d44acebbe5685957811280accfeeba6bbc
Erisk Aragol <earagol@andain.cl>
2018-01-31 11:29:21 -0300

    header , footer, principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
